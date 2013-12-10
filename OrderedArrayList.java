3f61fede36c387be136111415f5dc30ce545c7e5
Oliver Zhang <zhangoliver7@gmail.com>
2013-12-09 19:25:55 -0500

    HW 40

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -22,7 +22,7 @@ public class OrderedArrayList {
 
     // default constructor initializes instance variable _data
     public OrderedArrayList() {
-	_data = new ArrayList(10);
+	_data = new ArrayList(20);
     }
 
 
@@ -42,13 +42,13 @@ public class OrderedArrayList {
 
 
     public Comparable remove( int index ) { 
-        ArrayList<Comparable> temp = new ArrayList[_data.size() - 1];
+        ArrayList<Comparable> temp = new ArrayList(_data.size() - 1);
 	Comparable retComp = _data.get(index);
 	for( int i = 0; i < _data.size() - 1; i ++) {
 	    if (i >= index) 
-		temp[i] = _data.get(i + 1);
+		temp.set(i, _data.get(i + 1));
 	    if (i < index)
-		temp[i] = _data.get(i);
+		temp.set(i, _data.get(i));
 	}
 	_data = temp;
 	
@@ -64,8 +64,8 @@ public class OrderedArrayList {
 
     
     public Comparable get( int index ) { 
-        // *** YOUR IMPLEMENTATION HERE ***
-        return 0; //placeholder  
+        
+        return _data.get(index); //placeholder  
     }
 
 
@@ -75,7 +75,35 @@ public class OrderedArrayList {
     // maintains ascending order of elements
     // uses a linear search to find appropriate index
     public void addLinear(Comparable newVal) { 
-        // *** YOUR IMPLEMENTATION HERE ***
+        int i = 0;
+	int size1 = _data.size();
+	ArrayList<Comparable> temp = new ArrayList(size1 + 1);
+	temp.addAll(_data);
+	int size = temp.size();
+	if (_data.isEmpty()) 
+            temp.add(newVal);
+	    
+	else if (_data.get(0).compareTo(newVal) >= 0) 
+	    temp.add(0,newVal);
+	    
+	    
+	else if (_data.get(size - 1).compareTo(newVal) <= 0) 
+	    temp.add(size,newVal);
+	    
+	    
+	else {
+		while (i < size) {
+		    if (_data.get(i + 1).compareTo(newVal) >= 0 && _data.get(i).compareTo(newVal) <= 0) {
+		    	temp.add(i+1,newVal);
+			
+			break; }
+		    i++;
+		    
+		    
+	     }
+	}
+	
+	_data = temp;
     }
 
 
@@ -85,27 +113,72 @@ public class OrderedArrayList {
     // maintains ascending order of elements
     // uses a binary search to find appropriate index
     public void addBinary(Comparable newVal) { 
-        // *** YOUR IMPLEMENTATION HERE ***
+        int i = 0;
+	int size1 = _data.size();
+	ArrayList<Comparable> temp = new ArrayList(size1 + 1);
+	temp.addAll(_data);
+	int size = temp.size();
+	
+	if (_data.isEmpty()) 
+            temp.add(newVal);
+	    
+	else if (_data.get(0).compareTo(newVal) >= 0) 
+	    temp.add(0,newVal);
+	    
+	else if (_data.get(size - 1).compareTo(newVal) <= 0) 
+	    temp.add(size,newVal);
+
+	else {
+	    boolean found = false;
+	    
+	    int lo = 0;
+	    int hi = temp.size() - 1;
+	    i = (lo + hi) / 2;
+	    while (!found) {
+		if (temp.get(i + 1).compareTo(newVal) >= 0 && temp.get(i).compareTo(newVal) <= 0) {
+		    	temp.add(i+1,newVal);
+			
+			found = true; }
+		else if (temp.get(i + 1).compareTo(newVal) < 0)
+			lo = i + 1;
+		else if (temp.get(i).compareTo(newVal) > 0)
+			hi = i - 1;
+		i = (lo + hi) / 2;
+	   	
+	    }
+	}
+	_data = temp;
     }        
 
 
     // main method solely for testing purposes
     public static void main( String[] args ) {
 
-        /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        
         OrderedArrayList Franz = new OrderedArrayList();
-
+	/*ArrayList foo = new ArrayList();
+	System.out.println( foo );
+	for( int i = 0; i < 10; i++ ) 
+	    foo.add(i);
+	System.out.println( foo );*/
+	
         // testing linear search
-        for( int i = 0; i < 15; i++ )
-            Franz.addLinear( (int)( 50 * Math.random() ) );
-        System.out.println( Franz );
-
+	
+	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        for( int i = 0; i < 15; i++ ) {
+	    int addition = (int)( 50 * Math.random() );
+            Franz.addLinear( addition );
+	    
+            System.out.println( Franz + " new addition: " + addition);}
+	
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	
         // testing binary search
         Franz = new OrderedArrayList();
         for( int i = 0; i < 15; i++ ) 
             Franz.addBinary( (int)( 50 * Math.random() ) );
         System.out.println( Franz );
-           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+           
     }
 
 }//end class OrderedArrayList
